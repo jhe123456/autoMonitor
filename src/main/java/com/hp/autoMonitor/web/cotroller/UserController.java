53e465ce51a96342ad775f6aa88cd344e99b40d9
jhe123456 <jhe123456@gmail.com>
2016-08-17 15:53:50 +0800

    inteli

--- src/main/java/com/hp/autoMonitor/web/cotroller/UserController.java
+++ src/main/java/com/hp/autoMonitor/web/cotroller/UserController.java
@@ -12,22 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hp.autoMonitor.web.model.SysUser;
 import com.hp.autoMonitor.web.service.UserService;
 
- 
-    @RestController
-    @RequestMapping("/user")
-    public class UserController {
-      @Resource
-      private UserService userService;
-      
-      @RequestMapping("/showUser")
-      public String toIndex(HttpServletRequest request,Model model){
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @RequestMapping("/showUser")
+    public String toIndex(HttpServletRequest request, Model model) {
 //        int userId = Integer.parseInt(request.getParameter("id"));
 //        User user = this.userService.getUserById(userId);
-    	  List<SysUser> list=userService.getAllUsers();
+        List<SysUser> list = userService.getAllUsers();
         model.addAttribute("user", list);
+        System.out.println("***************");
         return "showUser";
-      }
     }
+}
 
  
  
